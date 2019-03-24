ec9ed8b20c483b71c2bed873ab4713d30c524b55
Jablan <joshuaablan@gmail.com>
2019-03-24 16:19:30 -0700

    Commit 4, finished D. The Farm

--- app.js
+++ app.js
@@ -65,3 +65,19 @@ console.log(`Hello my name is ${yourName}`)
   console.log(a !== b !== c);
   console.log(a === a || d);
   console.log(48 == '48');
+
+//   D. The farm
+// Declare a variable animal. Set it to be either "cow"
+//  or something else.
+let animal = "yup";
+// Write code that will print out "mooooo" if the it is equal to cow. 
+    if (animal === "cow") { 
+    console.log("Moooooooo")    
+     // Change your code so that if the variable animal is 
+    // anything other than a cow, it will print "Hey! You're not a cow."
+    } else {
+    console.log("Hey! You're not a cow!");
+ }
+
+// Commit.
+
