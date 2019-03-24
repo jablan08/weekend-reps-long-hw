ffd0b60ce2be2a09a002d0abef2e615f5cdfa81a
Jablan <joshuaablan@gmail.com>
2019-03-24 10:11:13 -0700

    Commit 3, finished C. Booleans

--- app.js
+++ app.js
@@ -42,3 +42,26 @@ let yourName = "Joshua Ablan"
 //  variable yourName 
 // so that it returns a new string with them concatenated.
 console.log(`Hello my name is ${yourName}`)
+
+// C. Booleans
+// Using the provided variable definitions,
+//  replace the blanks so that all log statements 
+//  print true in the console. Answers should be all be
+//   valid JS syntax and not weird things that 
+//   don't make sense but happen to print true to the console.
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a !== b);
+  console.log(c !== d);
+  console.log('Name' === 'Name');
+  console.log(true !== false);
+  console.log(false && false && false && false && false && false || true);
+  console.log(false === false);
+  console.log(e == 'Kevin');
+  console.log(a !== b !== c);
+  console.log(a === a || d);
+  console.log(48 == '48');
