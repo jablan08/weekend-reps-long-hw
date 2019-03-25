7763e569aa83a9fec60b730fcac87f2d884e7079
Jablan <joshuaablan@gmail.com>
2019-03-24 17:51:53 -0700

    Commit 6, finished Loops: A. The basics

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ let animal = "yup";
 
 // E. Driver's Ed
 // Make a variable that will hold a person's age. Be semantic.
-let age = null;
+let age = 19;
 // // Write code that will print out "Here are the keys",
 //  if the age is 16 years or older.
 if (age >= 16) {
@@ -95,3 +95,51 @@ if (age >= 16) {
 
 // // Commit.
 
+// II. Loops
+
+// A. The basics
+// Write a loop that will print out all the numbers from 0 to 10, inclusive.
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+} 
+// Write a loop that will print out all the numbers from 10 up to and including 400.
+for (let i = 10; i <= 400; i++) {
+    console.log(i);
+}
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000.
+for (let i = 12; i < 4000; i+=3) {
+    console.log(i);
+}
+// 🔴 Commit.
+
+// B. Get even
+// Print out the even numbers that are within the range of 1 - 100.
+
+// Adjust your code to add a message next to even numbers only that says: "<-- is an even number".
+
+// 🔴 Commit.
+
+// C. Give me Five
+// For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five.
+// Example Output:
+
+// I found a 5. High five!
+// I found a 10. High five!
+// Add to the code from above to print out "I found a number. Three is a crowd" if the number is a multiple of three.
+// Example Output:
+
+// I found a 3. Three is a crowd
+// I found a 5. High five!
+// I found a 6. Three is a crowd
+// I found a 9. Three is a crowd
+// I found a 10. High five!
+// 🔴 Commit.
+
+// D. Savings account
+// Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
+// Check your work! Your banck_account should have $55 in it.
+
+// You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+// Check your work! Your banck_account should have $10,100 in it.
+
+// 🔴 Commit.
