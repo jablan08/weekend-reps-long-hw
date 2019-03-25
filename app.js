821137401e1847ff3a9748f63757c36c284ed803
Jablan <joshuaablan@gmail.com>
2019-03-24 21:33:03 -0700

    Commit 18, finished Array partH

--- app.js
+++ app.js
@@ -313,22 +313,23 @@ const kristynsCloset = [
       'raybans'
     ]
   ];
-  // What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
-  
+  // What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, 
+  //log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+  console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!")
   // Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
-  
+  kristynsCloset.splice(6,0,"raybans");
   // Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
-  
+  kristynsCloset[5] = "stained knit hat";
   // Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirts array.
-  
+  thomsCloset[0][2];
   // In the same way, access one item from Thom's pants array.
-  
+  thomsCloset[1][1];
   // Access one item from Thom's accessories array.
-  
+  thomsCloset[2][1];
   // Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
-  
+    console.log(` Thom is lookin fire in his ${thomsCloset[0][3]} and his flashy ${thomsCloset[2][2]}.`)
   // Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
-  
+  thomsCloset[1][2] = "Footie Pajamas";
   // 🔴 Commit.
   
   
