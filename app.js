195162e71eaf317639a42b8511544696d00245cb
Jablan <joshuaablan@gmail.com>
2019-03-24 20:10:57 -0700

    Commit 14, finished Array part D.

--- app.js
+++ app.js
@@ -216,13 +216,14 @@ console.log(randomThings);
 
 // D. Change values
 // Given the following array const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"];
 
 // What would you write to access the 3rd element of the array?
-
+ourClass[2];
 // Change the value of "Github" to "Octocat"
-
+ourClass[4] = "Outcocat";
 // Add a new element, "Cloud City" to the array.
-
+ourClass.push("Cloud City");
 // 🔴 Commit.
 
 // E. Mix It Up
