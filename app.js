3f90ea8f3349f24f54af1e8d7b110dc95840e15d
Jablan <joshuaablan@gmail.com>
2019-03-24 18:36:44 -0700

    Commit 10, finshed Loops: E. Multiples of 3 and 5. I already done it

--- app.js
+++ app.js
@@ -167,3 +167,22 @@ for (let i =0; i <= 100; i++) {
     dank_Account += i * 2; 
 }; console.log(`Your bank account is dank! It has $${dank_Account} doll-hairs!`); 
 // 🔴 Commit.
+
+// E. Multiples of 3 and 5
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+//we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000. If a previous 
+// question you've done has helpful bits of code in it that partially 
+// solves this problem, look back at them.
+
+// You just solved Project Euler problem 1!
+
+// Are you having dejà vu? This just in! From the 
+//"Read the entire problem before you start" dept: 
+//This problem was on a previous assignment. You may skip it 
+//if you've already done it, just include a comment saying that you've 
+//already done it. If you've now done the problem twice, perhaps next 
+// time you'll read the whole problem before starting it.
+console.log("I have already done this!");
+// 🔴 Commit.
