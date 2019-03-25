aa73a41a5f952e591c1da5beead9333cd08ed603
Jablan <joshuaablan@gmail.com>
2019-03-24 21:38:15 -0700

    Commit 19, finished Functions part A. Already completed it.

--- app.js
+++ app.js
@@ -332,4 +332,135 @@ const kristynsCloset = [
   thomsCloset[1][2] = "Footie Pajamas";
   // 🔴 Commit.
   
+//   IV. Functions
+// A. printGreeting
+  console.log("I already complete printGreeting!");
+
+//   B. printCool
+//   Write a function printCool that accepts one parameter, 
+// name as an argument. The function should print the name and a message 
+// saying that that person is cool.
   
+//   console.log(printCool('Captain Reynolds'));
+//   => "Captain Reynolds is cool";
+  
+//   🔴 Commit.
+  
+//   C. calculateCube
+//   Write a function calculateCube that takes a single number 
+//   and prints the volume of a cube made from that number.
+  
+//   console.log(calculateCube(5));
+//   => 125
+  
+//   🔴 Commit.
+  
+//   D. isVowel
+//   Write a function isVowel that takes a character 
+//  (i.e. a string of length 1) and returns true if it 
+//  is a vowel, false otherwise. The vowel could be upper or lower case.
+  
+//   console.log(isVowel('a'));
+//   => true
+  
+//   🔴 Commit.
+  
+//   E. getTwoLengths
+//   Write a function getTwoLengths that accepts two parameters (strings). 
+//  The function should return an array of numbers where each number is 
+//  the length of the corresponding string.
+  
+//   console.log(getTwoLengths('Hank', 'Hippopopalous'));
+//   => [4, 13]
+  
+//   🔴 Commit.
+  
+//   F. getMultipleLengths
+//   Write a function getMultipleLengths that accepts a single parameter 
+//  as an argument: an array of strings. The function should return an array 
+//  of numbers where each number is the length of the corresponding string.
+  
+//   console.log(getMultipleLengths(['hello', 'what', 'is', 'up', 'dude']));
+//   => [5, 4, 2, 2, 4]
+  
+//   🔴 Commit.
+  
+//   G. maxOfThree
+//   Define a function maxOfThree that takes three numbers as arguments 
+//  and returns the largest of them. If all numbers are the same, it doesn't 
+//  matter which one is returned. If the two largest numbers are the same, one of them 
+//  should be returned.
+  
+//   console.log(maxOfThree(6, 9, 1));
+//   => 9
+  
+//   🔴 Commit.
+  
+//   H. printLongestWord
+//   Write a function printLongestWord that accepts a single 
+//  argument, an array of strings. The method should return the longest word in the 
+//  array. In case of a tie, the method should return the word that appears first in the array.
+  
+//   console.log(
+//     printLongestWord([
+//       'BoJack',
+//       'Princess',
+//       'Diane',
+//       'a',
+//       'Max',
+//       'Peanutbutter',
+//       'big',
+//       'Todd'
+//     ])
+//   );
+//   => "Peanutbutter"
+  
+//   🔴 Commit.
+  
+//   I. transmogrify
+//   Write a Javascript function called transmogrify. This function should accept three arguments,
+//   which you can assume will be numbers. Your function should return the "transmogrified" result.
+  
+//   The transmogrified result of three numbers is the product of the first two numbers,
+//   raised to the power of the third number.
+  
+//   For example, the transmogrified result of 5, 3, and 2 is (5 times 3) to the power of 2 is 225.
+  
+//   console.log(transmogrify(5, 3, 2));
+//   => 225
+  
+//   🔴 Commit.
+  
+//   J. reverseWordOrder v2
+//   Without using .split(), .reverse(), or .join(), write a function 
+//  reverseWordOrder that accepts a single argument, a string. The function should 
+//  return a string with the order of the words reversed. Don't worry about punctuation.
+  
+//   See if you can do it without googling.
+  
+//   Remember: Jim showed you today that you can index directly into a string:
+  
+//   'hello world'[6];
+//   => "w"
+  
+//   That and basic loops and variables and arrays are all you need to solve this 
+//  without the Array methods.
+  
+//   console.log(reverseWordOrder('Ishmael me Call'));
+//   => "Call me Ishmael"
+  
+//   console.log(reverseWordOrder('I use Lâncome on my comb'));
+//   => "comb my on Lâncome use I"
+  
+//   🔴 Commit.
+  
+//   K. Get down and dirty with Math.random()
+//   Write a function that will return a random integer between 1 and 10. Test it.
+//   Write a function that will return a random integer between 10 and 100. Test it.
+//   Write a function that will return a random number between 532 and 13267. Test it.
+//   Write a function that will return a random number between 1 and 10. Test it.
+//   Add a few more quotes to the quotes array from question III-B-1 above. 
+//  Write a function that will take an array as a parameter, and return a 
+//  random element from that array. Call your function a few times, 
+//  passing in the quotes array. Give it a nice semantic name like getRandomElement.
+//   🔴 Commit.
